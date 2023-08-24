fb1045a10d9f6f24c2eb99e7484bbe3e58ffb491
sun <sunyp0713@naver.com>
2023-08-24 15:20:59 +0900

    8/24 15:20

---
+++ realtime/src/main/java/com/sun/realtime/config/WebSocketConfig.java
@@ -0,0 +1,25 @@
+package com.sun.realtime.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+	
+	@Override
+	public void configureMessageBroker(MessageBrokerRegistry brokerRegistry) {
+
+		brokerRegistry.enableSimpleBroker("/topic");
+		brokerRegistry.setApplicationDestinationPrefixes("/app");	// 클라이언트가 보내야하는 위치
+	}
+
+	@Override
+	public void registerStompEndpoints(StompEndpointRegistry endpointRegistry) {
+		endpointRegistry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
+	}
+
+}
---
+++ realtime/src/main/java/com/sun/realtime/controller/MessageController.java
@@ -0,0 +1,22 @@
+package com.sun.realtime.controller;
+
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class MessageController {
+	
+	private final SimpMessagingTemplate messagingTemplate;
+
+	@PostMapping("/send")
+	public void send(@RequestBody String message) {
+		String destination = "/topic";
+		messagingTemplate.convertAndSend(destination, "message!!");
+	}
+
+}
